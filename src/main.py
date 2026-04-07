9f09779ddfa1b7a1ca4e57186b3de9ae3cc7aba7
yemeal <emelyalex@bk.ru>
2026-04-08 03:20:30 +0700

    security: restrict CORS origins

--- src/main.py
+++ src/main.py
@@ -14,10 +14,17 @@ app = FastAPI()
 
 app.include_router(short_url_router)
 
-# Настраиваем CORS для взаимодействия с фронтендом
+# Настраиваем CORS
+# В продакшене фронт и бэк работают на одном домене (через Nginx),
+# поэтому CORS нужен восновном только для локальной разработки.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",  # Локальный Vite фронтенд
+        "http://localhost:8000",  # Swagger/Локальный бэкенд
+        "https://шорти.рф",       # Продакшен домен (на всякий случай)
+        "https://xn--h1algi1a.xn--p1ai" # Punycode продакшен домена
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
