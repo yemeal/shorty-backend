7a9012d8a3c3865a34d2c081ca278d066501b0db
yemeal <emelyalex@bk.ru>
2026-04-10 00:26:57 +0700

    chore: add exceptions for users and tokens

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,3 +1,4 @@
+# region Short URL exceptions
 class SlugException(Exception):
     pass
 
@@ -12,11 +13,51 @@ class ShortUrlGenerationException(SlugException):
 
 class LongUrlNotFoundException(SlugException):
     pass
+# endregion Short URL exceptions
 
 
+# region Retry exceptions
 class RetryException(Exception):
     pass
 
 
 class RetriesAmountExceeded(RetryException):
     pass
+# endregion Retry exceptions
+
+
+# region User exceptions
+class UserException(Exception):
+    pass
+
+class IncorrectEmailOrPasswordException(UserException):
+    pass
+
+class UserExistsException(UserException):
+    pass
+
+class UserWithEmailExistsException(UserExistsException):
+    pass
+
+class UserWithUsernameExistsException(UserExistsException):
+    pass
+
+# endregion User exceptions
+
+
+# region Token/Auth exceptions
+class TokenException(Exception):
+    pass
+
+
+class TokenExpiredException(TokenException):
+    pass
+
+
+class InvalidTokenTypeException(TokenException):
+    pass
+
+
+class TokenNoSubException(TokenException):
+    pass
+# endregion Token/Auth exceptions
