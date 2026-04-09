88ec0f14fd7f9e41140f52c673b6002c734a9a68
yemeal <emelyalex@bk.ru>
2026-04-10 00:27:55 +0700

    chore: update config for token generation and password hashing

--- src/core/config.py
+++ src/core/config.py
@@ -30,3 +30,11 @@ RESERVED_SLUGS = {
     "апи",
     "админ",
 }
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
