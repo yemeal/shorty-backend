bae7c0ff01d1cb5bdb931374d4431d75892ab059
yemeal <95481119+yemeal@users.noreply.github.com>
2026-04-16 01:36:27 +0700

    fix: add UserProfileServiceProtocol

--- src/utils/protocols/user_profile.py
+++ src/utils/protocols/user_profile.py
@@ -1,5 +1,17 @@
 from typing import Protocol
+from uuid import UUID
+
+from src.models import UserProfile
+from src.schemas.user_profile import UserProfileUpdate
 
 
 class UserProfileServiceProtocol(Protocol):
+    async def update_user_profile(
+        self,
+        user_id: UUID,
+        user_profile_update: UserProfileUpdate,
+    ) -> UserProfile: ...
+
+    async def get_user_profile_by_user_id(self, user_id: UUID) -> UserProfile: ...
+
     
